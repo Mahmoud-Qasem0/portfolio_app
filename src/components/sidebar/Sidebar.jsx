ec875d9dcef1fc5e9a453160e84ba788f47847a8
MahmoudQasem <mahmoudqasem1@hotmail.com>
2024-12-08 08:15:49 +0200

    edit sidebar toggle

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -35,21 +35,21 @@ function Sidebar() {
             </p>
             <ul className="m-0 p-0 list-unstyled">
               <li className="">
-                <Link to="home" className="active nav-link">
+                <Link to="home" className="active nav-link" onClick={handleToggle} >
                   Home
                 </Link>
               </li>
               <li className="">
-                <Link to="about" className="active nav-link">
+                <Link to="about" className="active nav-link" onClick={handleToggle} >
                   About
                 </Link>
-                <Link to="skills" className="active nav-link">
+                <Link to="skills" className="active nav-link" onClick={handleToggle} >
                   Skills
                 </Link>
-                <Link to="experience" className="active nav-link">
+                <Link to="experience" className="active nav-link" onClick={handleToggle} >
                   Experience
                 </Link>
-                <Link to="work" className="active nav-link">
+                <Link to="work" className="active nav-link" onClick={handleToggle} >
                   Work
                 </Link>
               </li>
